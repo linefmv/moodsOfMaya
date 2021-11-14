d0262c1d85692fae902d320aac6bcedad3b1ad1e
Aline Viana <alinemaciel018@gmail.com>
2021-11-14 00:07:45 -0300

    exercicio da semana

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
-
+import Card from './components/Card';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      
+        <div>
+        <h1> Maya the dog 🐶</h1>
+        <p className="description"> She is the best partner in the word,
+           and we are best friends. </p>
+        </div>
+      <Card/>
       </header>
     </div>
   );
---
+++ src/data/database.js
@@ -0,0 +1,33 @@
+const moodsOfMaya = [
+    {
+      id: 1,
+      nameOfMood: "The Crazy Dog",
+      type: "Madness",
+      pic: "./img/crazy.jpeg"
+  
+    },
+    {
+      id: 2,
+      nameOfMood: "The Sleep One",
+      type: "lazy",
+      pic: "./img/sleep.jpeg"
+  
+    },
+    {
+      id: 3,
+      nameOfMood: "Funny Girl",
+      type: "comedy",
+      pic: "./img/funny.jpeg"
+  
+    },
+    {
+      id: 4,
+      nameOfMood: "Gossip",
+      type: "tmz",
+      pic: "./img/gossip.jpeg"
+  
+    }
+  
+  ]
+  
+  export default moodsOfMaya
