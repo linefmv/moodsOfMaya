7f83b743f2906ee2ad661b737f8718c3b67fbd9f
Aline Viana <alinemaciel018@gmail.com>
2021-11-15 16:51:49 -0300

    add styles

---
+++ src/components/Card/styles.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components'; 
+
+export const SelectedMood = styled.section`
+ display: flex;
+  overflow-x: auto;
+  scroll-behavior: smooth;
+`;
+
+export const ImageMood = styled.img`
+width: 100%;
+height: 50%;
+  
+`;
+export const PhotoAndDescription = styled.div`
+display: flex;
+flex-direction: column;
+`
+
+export const MoodOfDay = styled.h2`
+  display: flex;
+`;
+
+export const TypeOfMood = styled.h3`
+  display: flex;
+`;
