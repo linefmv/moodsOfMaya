9edf55f49e9c56943350223907ec79e13435ad8e
Aline Viana <alinemaciel018@gmail.com>
2021-11-19 08:07:02 -0300

    improve style

--- src/components/TitleAndDescription/styles.js
+++ src/components/TitleAndDescription/styles.js
@@ -1,17 +1,15 @@
 import styled from 'styled-components'; 
 
-export const header = styled.header`
+export const Header = styled.header`
   display: flex;
   flex-direction: column;
-  align-items: center;
-
+   align-items: center;
+   justify-content: center;
 `;
 export const Title = styled.h1`
-  font-size: 1.5rem;
-  align-self: center;
+  font-size: 2rem;
 `;
 
 export const  Description = styled.p`
    font-size: 1rem;
-
 `;
